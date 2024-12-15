6fc067d638a082bac9a4b66df481c15379c2908c
Matt7046 <matteo.santangelo@colorsdev.tech>
2024-12-15 10:17:24 +0100

    modifica servizi

--- src/main/java/com/webapp/controller/AboutController.java
+++ src/main/java/com/webapp/controller/AboutController.java
@@ -10,10 +10,13 @@ import com.webapp.mapper.SubPromiseMapper;
 import com.webapp.service.SubPromiseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("api/about")
@@ -31,4 +34,24 @@ public class AboutController {
    
         return new ResponseDTO(item, HttpStatus.OK);
     }
+
+  @PostMapping("/dati")
+public ResponseEntity<ResponseDTO> saveSubPromise(@RequestBody SubPromiseDTO subPromiseDTO) {
+    try {
+        // Salva i dati e ottieni l'ID o l'oggetto salvato
+        Long itemId = subPromiseService.saveSubPromise(subPromiseDTO);
+
+        // Crea una risposta
+        ResponseDTO response = new ResponseDTO(itemId, HttpStatus.OK);
+
+        // Ritorna una ResponseEntity con lo status HTTP
+        return ResponseEntity.ok(response);
+
+    } catch (Exception e) {
+        // Gestione degli errori: puoi personalizzarlo in base al tuo scenario
+        ResponseDTO errorResponse = new ResponseDTO(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+}
+
 }
