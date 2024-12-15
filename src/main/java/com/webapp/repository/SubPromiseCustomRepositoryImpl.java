59382db8484479e715a55e0091199f5ba8b7b16b
Matt7046 <matteo.santangelo@colorsdev.tech>
2024-12-15 13:19:03 +0100

    correzione repository

--- src/main/java/com/webapp/repository/SubPromiseCustomRepositoryImpl.java
+++ src/main/java/com/webapp/repository/SubPromiseCustomRepositoryImpl.java
@@ -5,43 +5,41 @@ import org.springframework.context.annotation.Lazy;
 
 import com.webapp.data.SubPromise;
 import com.webapp.dto.SubPromiseDTO;
-import com.webapp.service.SubPromiseService;
 
 public class SubPromiseCustomRepositoryImpl implements SubPromiseCustomRepository {
 
     @Lazy
     @Autowired
-    private SubPromiseService subPromiseService;
+    private SubPromiseRepository subPromiseRepository;
 
     public String saveSubPromise(SubPromiseDTO subPromiseDTO) {
         // Verifica se esiste già un documento con l'identificativo
         SubPromise existingPromise = null;
         if (subPromiseDTO.get_id() != null) {
-            existingPromise = subPromiseService.findByIdentificativo(subPromiseDTO.get_id());
+            existingPromise = subPromiseRepository.findByIdentificativo(subPromiseDTO.get_id());
         }
 
         if (existingPromise != null) {
             // Se esiste, aggiorna i campi
-            SubPromiseDTO newPromise = new SubPromiseDTO();
+            SubPromise newPromise = new SubPromise();
             newPromise.set_id(existingPromise.get_id());
             newPromise.setNome(subPromiseDTO.getNome());
             newPromise.setSubTesto(subPromiseDTO.getSubTesto());
-            existingPromise = subPromiseService.save(newPromise);
+            existingPromise = subPromiseRepository.save(newPromise);
 
             return existingPromise.get_id();// Restituisci l'ID aggiornato
         } else {
-            SubPromise newPromiseR = new SubPromise();
             // Se non esiste, crea un nuovo documento
-            SubPromiseDTO newPromise = new SubPromiseDTO();
+            SubPromise newPromise = new SubPromise();
             newPromise.setNome(subPromiseDTO.getNome());
             newPromise.setSubTesto(subPromiseDTO.getSubTesto());
             try {
-                newPromiseR = subPromiseService.save(newPromise);
+                newPromise = subPromiseRepository.save(newPromise);
             } catch (Exception e) {
                 // Log dell'errore per un'analisi successiva
                 System.err.println("Errore durante il salvataggio del documento SubPromise: " + e.getMessage());
             }
-            return newPromiseR.get_id(); // Restituisci l'ID del nuovo documento
+            return newPromise.get_id(); // Restituisci l'ID del nuovo documento
         }
     }
 }
