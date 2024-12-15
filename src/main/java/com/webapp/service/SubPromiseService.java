780020e80331c38e411a0f8e91582bea7aa90cf7
Matt7046 <matteo.santangelo@colorsdev.tech>
2024-12-15 11:09:47 +0100

    aggiunto servizio nuovo

--- src/main/java/com/webapp/service/SubPromiseService.java
+++ src/main/java/com/webapp/service/SubPromiseService.java
@@ -1,7 +1,5 @@
 package com.webapp.service;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -14,6 +12,7 @@ import com.webapp.dto.SubPromiseDTO;
 import com.webapp.repository.SubPromiseRepository;
 
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class SubPromiseService {
@@ -25,40 +24,47 @@ public class SubPromiseService {
 
     public List<SubPromise> findAll() {
         return subPromiseRepository.findAll();
-    }  
-    
+    }
+
     public SubPromise findByIdentificativo(Long identificativo) {
-        
+
         return subPromiseRepository.findByIdentificativo(identificativo);
     }
 
     public Long deleteByIdentificativo(Long identificativo) {
-        
+
         return subPromiseRepository.deleteByIdentificativo(identificativo);
     }
 
     public Long saveSubPromise(SubPromiseDTO subPromiseDTO) {
         // Verifica se esiste già un documento con l'identificativo
-        SubPromise existingPromise = subPromiseRepository.findByIdentificativo(subPromiseDTO.getIdentificativo());
-        
+        SubPromise existingPromise = null;
+        if (subPromiseDTO.getIdentificativo() != null) {
+            existingPromise = subPromiseRepository.findByIdentificativo(subPromiseDTO.getIdentificativo());
+        }
+
         if (existingPromise != null) {
             // Se esiste, aggiorna i campi
             existingPromise.setNome(subPromiseDTO.getNome());
             existingPromise.setSubTesto(subPromiseDTO.getSubTesto());
-            subPromiseRepository.save(existingPromise); // Salva i cambiamenti
             return existingPromise.getIdentificativo(); // Restituisci l'ID aggiornato
         } else {
+            Random random = new Random();
+
+            // Generating a random integer
+            Long randomNumber = random.nextLong(1000000);
             // Se non esiste, crea un nuovo documento
             SubPromise newPromise = new SubPromise();
-            newPromise.setIdentificativo(subPromiseDTO.getIdentificativo());
+            newPromise.setIdentificativo(randomNumber);
             newPromise.setNome(subPromiseDTO.getNome());
             newPromise.setSubTesto(subPromiseDTO.getSubTesto());
-            subPromiseRepository.save(newPromise); // Salva il nuovo oggetto
+            try {
+                newPromise = subPromiseRepository.save(newPromise);
+            } catch (Exception e) {
+                // Log dell'errore per un'analisi successiva
+                System.err.println("Errore durante il salvataggio del documento SubPromise: " + e.getMessage());
+            }
             return newPromise.getIdentificativo(); // Restituisci l'ID del nuovo documento
         }
     }
 }
-
-
-
-
