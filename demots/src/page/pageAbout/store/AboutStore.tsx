31bdf2da0cda737cbb529881871bc42d50e68d37
Matt7046 <matteo.santangelo@colorsdev.tech>
2024-12-16 16:37:00 +0100

    spostamento cartelle

--- demots/src/page/pageAbout/store/AboutStore.tsx
+++ demots/src/page/pageAbout/store/AboutStore.tsx
@@ -1,5 +1,5 @@
 import { observer } from 'mobx-react';
-import BaseStore from '../../general/BaseStore';
+import BaseStore from '../../../general/BaseStore';
 
 export class AboutStore extends BaseStore {
   
