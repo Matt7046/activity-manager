37f1773d76e4ec32a92bdcd65ae983b9782ef4af
Matt7046 <matteo.santangelo@colorsdev.tech>
2024-12-16 16:44:54 +0100

    fix

--- demots/src/components/ACnomeDisplay/NomeDisplay.tsx
+++ demots/src/components/ACnomeDisplay/NomeDisplay.tsx
@@ -4,15 +4,15 @@ const NomeDisplay = observer((props: {
   key: number;
   value: string;
   rowIndex: number; handleClick: () => void
-  
+
 }) => {
- return ( 
-  <>
-    <h2 id={'displayer-' + props.rowIndex.toString()} onClick={props.handleClick}>
-      {props.value} {/* Mostra il testo dallo store */}
-    </h2>
-  </>
-);
+  return (
+    <>
+      <h2 id={'displayer-' + props.rowIndex.toString()} onClick={props.handleClick}>
+        {props.value} {/* Mostra il testo dallo store */}
+      </h2>
+    </>
+  );
 
 })
 export default NomeDisplay;
