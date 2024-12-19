a4bd51141f443fa3c4e0c4b9d0e1e78d4ef89c8b
Matt7046 <matteo.santangelo@colorsdev.tech>
2024-12-20 00:06:25 +0100

    aggancio backend

--- demots/src/page/page-about/service/AboutService.tsx
+++ demots/src/page/page-about/service/AboutService.tsx
@@ -5,7 +5,9 @@ import { deleteData, postData } from "../../../general/AxiosService";
 export const deleteAboutById = async (_id: string, funzioneErrore?:any) => {
   try {
     _id = _id ? _id : '-1';
-    const path = `/about/${_id}`;
+    const apiUrl = process.env.REACT_APP_API_URL; // Ottieni l'URL dal file .env
+
+    const path = `${apiUrl}/api/about/${_id}`;
     const data = await deleteData(path); // Endpoint dell'API
     console.log('Dati ricevuti:', data);
     return data;
@@ -20,7 +22,9 @@ export const deleteAboutById = async (_id: string, funzioneErrore?:any) => {
 
 export const saveAboutById = async (_id: string, about: any, funzioneErrore?: any) => {
   try {
-    const path = `/about/dati`;
+    const apiUrl = process.env.REACT_APP_API_URL; // Ottieni l'URL dal file .env
+
+    const path = `${apiUrl}/api/about/dati`;
     const data = await postData(path, about); // Endpoint dell'API
     console.log('Dati ricevuti:', data);
     return data;
