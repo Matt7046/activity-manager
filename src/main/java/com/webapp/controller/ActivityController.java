5b86b0a1df0842f4fb1b1498a64ee4983b905afc
Matt7046 <matteo.santangelo@colorsdev.tech>
2024-12-19 13:15:12 +0100

    rename file

--- src/main/java/com/webapp/controller/ActivityController.java
+++ src/main/java/com/webapp/controller/ActivityController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-@RequestMapping("api/Activity")
+@RequestMapping("api/activity")
 @CrossOrigin(origins = "http://localhost:3000")
 public class ActivityController {
 
