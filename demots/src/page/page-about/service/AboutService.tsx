a743b510304a6c577f872ea37e5719fde3b4aecf
Matt7046 <matteo.santangelo@colorsdev.tech>
2024-12-20 16:05:04 +0100

    configurazione locale

--- demots/src/page/page-about/service/AboutService.tsx
+++ demots/src/page/page-about/service/AboutService.tsx
@@ -7,7 +7,7 @@ export const deleteAboutById = async (_id: string, funzioneErrore?:any) => {
     _id = _id ? _id : '-1';
     const apiUrl = process.env.REACT_APP_API_URL; // Ottieni l'URL dal file .env
 
-    const path = `${apiUrl}/api/about/${_id}`;
+    const path = `about/${_id}`;
     const data = await deleteData(path); // Endpoint dell'API
     console.log('Dati ricevuti:', data);
     return data;
@@ -24,7 +24,7 @@ export const saveAboutById = async (_id: string, about: any, funzioneErrore?: an
   try {
     const apiUrl = process.env.REACT_APP_API_URL; // Ottieni l'URL dal file .env
 
-    const path = `${apiUrl}/api/about/dati`;
+    const path = `about/dati`;
     const data = await postData(path, about); // Endpoint dell'API
     console.log('Dati ricevuti:', data);
     return data;
--- demots/src/page/page-activity/service/ActivityService.tsx
+++ demots/src/page/page-activity/service/ActivityService.tsx
@@ -2,10 +2,10 @@ import { getData } from "../../../general/AxiosService";
 
 export const fetchDataActivity = async (funzioneErrore?: any) => {
   try {
-    const apiUrl = process.env.REACT_APP_API_URL_LOCALE ; // Ottieni l'URL dal file .env
+    const apiUrl = process.env.REACT_APP_API_URL ; // Ottieni l'URL dal file .env
     //const apiUrl = process.env.REACT_APP_API_URL // Ottieni l'URL dal file .env
 
-    const data = await getData(`${apiUrl}/activity`); // Usa l'URL dinamico
+    const data = await getData(`activity`); // Usa l'URL dinamico
     console.log('Dati ricevuti:', data);
     return data;
   } catch (error) {
@@ -22,7 +22,7 @@ export const fetchDataActivityById = async (_id: string, funzioneErrore?: any) =
     _id = _id ? _id : '-1';
     const apiUrl = process.env.REACT_APP_API_URL; // Ottieni l'URL dal file .env
 
-    const path = `${apiUrl}/api/activity/${_id}`;
+    const path = `activity/${_id}`;
     const data = await getData(path); // Endpoint dell'API
     console.log('Dati ricevuti:', data);
     return data;
