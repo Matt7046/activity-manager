9628ff2379326bd3b6b0dd6fdecb6f99bcf14b3b
Matt7046 <matteo.santangelo@colorsdev.tech>
2024-12-22 18:08:14 +0100

    componente cardGrid

--- demots/src/page/page-points/service/PointsService.tsx
+++ demots/src/page/page-points/service/PointsService.tsx
@@ -4,9 +4,21 @@ import { getData, postData } from "../../../general/AxiosService";
 
 export const findByEmail = async (email: string, funzioneErrore?: any) => {
   try {
-    const apiUrl = process.env.REACT_APP_API_URL; // Ottieni l'URL dal file .env
-
     const path = `points`;
+    const data = await postData(path, { email }); // Endpoint dell'API
+    console.log('Dati ricevuti:', data);
+    return data;
+  } catch (error) {
+    console.error('Errore durante il recupero dei dati:', error);
+    if (funzioneErrore) {
+      funzioneErrore();
+    }
+  }
+};
+
+export const findLogByEmail = async (email: string, funzioneErrore?: any) => {
+  try {
+    const path = `points/log`;
     const data = await postData(path, { email }); // Endpoint dell'API
     console.log('Dati ricevuti:', data);
     return data;
@@ -20,8 +32,6 @@ export const findByEmail = async (email: string, funzioneErrore?: any) => {
 
 export const savePointsById = async (user: any, funzioneErrore?: any) => {
   try {
-    const apiUrl = process.env.REACT_APP_API_URL; // Ottieni l'URL dal file .env
-
     const path = `points/dati`;
     const data = await postData(path, user); // Endpoint dell'API
     console.log('Dati ricevuti:', data);
