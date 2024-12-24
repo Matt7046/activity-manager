c4bedd6ad42519eec669833c8aef8321f2a2397f
Matt7046 <matteo.santangelo@colorsdev.tech>
2024-12-24 15:39:09 +0100

    refactoring

--- demots/src/page/page-activity/ActivityContent.tsx
+++ demots/src/page/page-activity/ActivityContent.tsx
@@ -3,19 +3,17 @@ import activityStore from "./store/ActivityStore";  // Importa lo store
 import "./ActivityContent.css";
 import { useNavigate } from "react-router-dom";
 import { fetchDataActivityById } from "./service/ActivityService";
-import { openDetail, toggleVisibility } from "./ActivityFunc";
+import { navigateToAboutPage, openDetail, toggleVisibility } from "./ActivityFunc";
 import { navigateRouting, sezioniMenu, sezioniMenuIniziale, showError } from "../../App";
 import { Pulsante } from "../../components/msbutton/Button";
-import Schedule from "../../components/msschedule/Schedule";
+import Schedule, { MsSchedule } from "../../components/msschedule/Schedule";
 import { myDisplayer } from "../../general/Utils";
 
 
 
 const ActivityContent: React.FC<any> = ({
+  responseSchedule,
   user,
-  identificativo,
-  visibiityButton // Proprietà opzionale per la sottolineatura
-
 }) => {
 
   const navigate = useNavigate(); // Ottieni la funzione di navigazione
@@ -26,23 +24,46 @@ const ActivityContent: React.FC<any> = ({
   const [labelText] = React.useState('Nessun dato aggiuntivo'); // Stato dinamico per il testo della label
   const [open, setOpen] = useState(false); // Controlla la visibilità del messaggio
   const [errors, setErrors] = useState('Si è verificato un errore! Controlla i dettagli.')
-  const titleLabel = activityStore.testo.find(item => item._id === identificativo)?.nome || '';
   const [isVertical, setIsVertical] = useState<boolean>(window.innerHeight > window.innerWidth);
+  const [loading, setLoading] = useState(false);
+  const [visibilityButton, setVisibilityButton] = useState<boolean>(true); // Stato iniziale vuoto
   const flex = isVertical ? 'flex-start' : 'flex-end';
 
+
+
+
+
+  const pulsanteNew: Pulsante = {
+    icona: 'fas fa-plus',
+    funzione: () => navigateToAboutPage(navigate),
+    callBackEnd : () =>{ },
+    nome: 'new',
+    title: 'Nuovo documento'
+  };;  
+
   const pulsanteRed: Pulsante = {
-    icona: 'fas fa-download',
-    funzione: () => toggleVisibility(identificativo, () => handleClickMostraLabel(identificativo)), // Passi la funzione direttamente
-    nome: 'red',
-    title: 'Carica sottotesto'
-  };
+    icona: 'fas fa-download', 
+   // funzione: () => toggleVisibility('',fetchDataById('', () => showError(setOpen, setErrors)) ), // Passi la funzione direttamente
+   callBackEnd : (_id: string) =>fetchDataActivityById(_id, () => showError(setOpen, setErrors)),
+   nome: 'red', 
+    title: 'Carica sottotesto',
+    visibilityButton: visibilityButton
+  } as any
 
   const pulsanteBlue: Pulsante = {
     icona: 'fas fa-eye',
-    funzione: () => openDetail(identificativo, () => componentDidMount(identificativo)), // Passi la funzione direttamente
+    funzione: (_id: string) => openDetail(_id, () => componentDidMount(_id)), // Passi la funzione direttamente
+  //  callBackEnd : (_id: string) => componentDidMount(_id),
     nome: 'blue',
     title: 'Apri dettaglio'
-  };
+  } as any
+
+  const pulsantiVisibili = isVertical ? [pulsanteNew, pulsanteBlue] : [pulsanteNew, pulsanteRed, pulsanteBlue]
+
+
+
+
+
   useEffect(() => {
 
     const handleResize = () => {
@@ -57,10 +78,10 @@ const ActivityContent: React.FC<any> = ({
 
   const componentDidMount = (_id: string) => {
     // Effettua la chiamata GET quando il componente è montato
-    fetchDataActivityById(_id, () => showError(setOpen, setErrors))
+    fetchDataActivityById(_id, () => showError(setOpen, setErrors), setLoading)
       .then((response) => {
         if (response.status === 'OK') {
-          activityStore.setTestoById(_id, response.testo);  
+          activityStore.setTestoById(_id, response.testo);
           navigateRouting(navigate, `about`, { _id })
           console.log('Dati ricevuti:', response);
         } else {
@@ -72,33 +93,30 @@ const ActivityContent: React.FC<any> = ({
         console.error('Errore durante il recupero dei dati:', error);
       });
   }
-
-  const handleClickMostraLabel = (_id: string) => {
-
-    return fetchDataActivityById(_id, () => showError(setOpen, setErrors)).then((response) => {
-      if (response) {
-        const subTesto = response.testo.subTesto && response.testo.subTesto !== '' ? response.testo.subTesto : 'Nessun dato aggiuntivo';
-        return myDisplayer("label-" + _id, subTesto)
-      }
-    })
-  }
-
   const handleClose = () => {
     setOpen(false);
   };
 
-  pulsanteRed.funzione = () => toggleVisibility(identificativo, () => handleClickMostraLabel(identificativo));
-  pulsanteBlue.funzione = () => openDetail(identificativo, () => componentDidMount(identificativo));
+
+
+  const scheduler: MsSchedule = {
+    justifyContent: flex,
+    onClose: onclose,
+    handleClose: handleClose,
+    schedule: responseSchedule,
+    errors: errors,
+    visibilityButton:isVertical,
+    open: open,
+    pulsanti: pulsantiVisibili
+  }
 
   // Crea l'array dei pulsanti in base all'orientamento
-  const pulsantiVisibili = isVertical ? [pulsanteBlue] : [pulsanteRed, pulsanteBlue]
 
   return (
     <>
-      <div id="schedlule">
-        <Schedule justifyContent={flex} onClose={onclose} handleClose={handleClose} identificativo={identificativo}
-          titleLabel={titleLabel} subTitleLabel={labelText} errors={errors} visibilityButton={visibiityButton}
-          open={open} pulsanti={pulsantiVisibili} />
+      <div id="schedule">
+        <Schedule
+          schedule={scheduler} />
       </div>
     </>
   );
