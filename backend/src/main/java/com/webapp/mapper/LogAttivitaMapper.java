798360ffddc38bb213a8acef462761afe02e0a8c
Matt7046 <matteo.santangelo@colorsdev.tech>
2024-12-27 09:01:33 +0100

    card log

--- backend/src/main/java/com/webapp/mapper/LogAttivitaMapper.java
+++ backend/src/main/java/com/webapp/mapper/LogAttivitaMapper.java
@@ -2,6 +2,7 @@ package com.webapp.mapper;
 
 import org.mapstruct.factory.Mappers;
 import com.webapp.data.LogAttivita;
+import com.webapp.dto.LogAttivitaDTO;
 import com.webapp.dto.ActivityDTO;
 
 import org.mapstruct.*;
@@ -15,6 +16,13 @@ public interface LogAttivitaMapper {
     @Mapping(target = "_id", ignore = true) // Ignora il campo _id
     @Mapping(target = "logAttivita", ignore = true) // Inizialmente ignoriamo attivitaSvolte
     ActivityDTO toCastDTO(LogAttivita logAttivita);
+
+      // Da Entity a DTO
+    LogAttivitaDTO toDTO(LogAttivita logAttivita);
+    
+
+    // Da DTO a Entity
+    LogAttivita fromDTO(LogAttivitaDTO logAttivitaDto);
 
     // Metodo per trasformare "attivitaSvolte" e popolare il DTO
     @AfterMapping
