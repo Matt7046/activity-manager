ca8aaf5f4a5ab66670e7d42f3d38ddd503786a76
Matt7046 <matteo.santangelo@colorsdev.tech>
2024-12-27 13:55:48 +0100

    gestione points

---
+++ backend/src/main/java/Exception/ArithmeticCustomException.java
@@ -0,0 +1,12 @@
+package Exception;
+
+
+public class ArithmeticCustomException extends ArithmeticException {
+    public ArithmeticCustomException() {
+        super("Operazione non possibile");
+    }
+    public ArithmeticCustomException(String message) {
+        super(message);
+    }  
+
+}
