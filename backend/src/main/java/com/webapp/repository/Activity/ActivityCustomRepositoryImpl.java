1a0f16d544fa896ab5f0abbe6f391c7c7e44cc63
Matt7046 <matteo.santangelo@colorsdev.tech>
2024-12-28 17:42:04 +0100

    refactoring backend

--- backend/src/main/java/com/webapp/repository/Activity/ActivityCustomRepositoryImpl.java
+++ backend/src/main/java/com/webapp/repository/Activity/ActivityCustomRepositoryImpl.java
@@ -12,29 +12,29 @@ public class ActivityCustomRepositoryImpl implements ActivityCustomRepository {
     @Autowired
     private ActivityRepository ActivityRepository;
 
-    public String saveActivity(ActivityDTO activityDTO) {
+    public String saveActivity(Activity activity) {
         // Verifica se esiste già un documento con l'identificativo
         Activity existingActivity = null;
-        if (activityDTO.get_id() != null) {
-            existingActivity = ActivityRepository.findByIdentificativo(activityDTO.get_id());
+        if (activity.get_id() != null) {
+            existingActivity = ActivityRepository.findByIdentificativo(activity.get_id());
         }
 
         if (existingActivity != null) {
             // Se esiste, aggiorna i campi
             Activity newActivity = new Activity();
             newActivity.set_id(existingActivity.get_id());
-            newActivity.setNome(activityDTO.getNome());
-            newActivity.setSubTesto(activityDTO.getSubTesto());
-            newActivity.setPoints(activityDTO.getPoints());
+            newActivity.setNome(activity.getNome());
+            newActivity.setSubTesto(activity.getSubTesto());
+            newActivity.setPoints(activity.getPoints());
             existingActivity = ActivityRepository.save(newActivity);
 
             return existingActivity.get_id();// Restituisci l'ID aggiornato
         } else {
             // Se non esiste, crea un nuovo documento
             Activity newActivity = new Activity();
-            newActivity.setNome(activityDTO.getNome());
-            newActivity.setSubTesto(activityDTO.getSubTesto());
-            newActivity.setPoints(activityDTO.getPoints());
+            newActivity.setNome(activity.getNome());
+            newActivity.setSubTesto(activity.getSubTesto());
+            newActivity.setPoints(activity.getPoints());
             newActivity = ActivityRepository.save(newActivity);
             return newActivity.get_id(); // Restituisci l'ID del nuovo documento
         }
