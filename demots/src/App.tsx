000c547a3ec3354481480d572a32023ca90090ad
Matt7046 <matteo.santangelo@colorsdev.tech>
2024-12-29 14:48:00 +0100

    corretto segnalazione errori

--- demots/src/App.tsx
+++ demots/src/App.tsx
@@ -1,7 +1,7 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { Alert, Button as ButtonMui, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, Grid, Snackbar, TextField } from '@mui/material';
 import { GoogleLogin, GoogleOAuthProvider, useGoogleLogin } from '@react-oauth/google';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { NavigateFunction, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 import { MenuLaterale } from './components/msdrawer/Drawer';
@@ -33,12 +33,13 @@ const GoogleAuthComponent = () => {
   const navigate = useNavigate();  // Qui chiami useNavigate correttamente all'interno di un componente
   const [user, setUser] = useState<any>(null);
   const [open, setOpen] = useState(false); // Controlla la visibilità del messaggio
-  const [errors, setErrors] = useState('')
   const [loading, setLoading] = useState(false);
   const [simulated, setSimulated] = useState(false);
   const [title, setTitle] = useState("");
   const [openD, setOpenD] = useState(false); // Stato per la dialog
   const [email, setEmail] = useState('parentemail@simulated.it'); // Stato per l'email
+    const [message, setMessage] = React.useState<TypeMessage>({}); // Lo stato è un array di stringhe
+  
   const [userData, setUserData] = useState({
     name: "Simulated User",
     email: "user@simulated.com",
@@ -104,17 +105,19 @@ const GoogleAuthComponent = () => {
       handleOpenD();
     } else {
       setUser({ ...userData, type: type });
-      saveUserData({ ...userData, type: type }, setLoading);
-    }
-    navigateRouting(navigate, `activity`, {})
+      saveUserData({ ...userData, type: type }, setLoading)
+    }    
   }
 
 
 
 
-  const saveUserData = (userData: any, setLoading: any): void => {
+  const saveUserData = (userData: any, setLoading: any): Promise<any> => {
     //  const utente = { email: userData.email, type: userData.type }
-    savePoints(userData, () => showMessage(setOpen, setErrors), setLoading)
+    return savePoints(userData, () => showMessage(setOpen, setMessage), setLoading).then((x) => {
+      console.log('User Data:', x); // Logga i dati utente per il debug
+       navigateRouting(navigate, `activity`, { })
+    })
   }
 
   // Funzione per ottenere i dati utente
@@ -137,9 +140,7 @@ const GoogleAuthComponent = () => {
       if (userDataResponse.ok) {
         const userData = await userDataResponse.json();
         setUser({ ...userData, type: 1 });
-        saveUserData({ ...userData, type: 1 }, setLoading);
-        console.log('User Data:', userData); // Logga i dati utente per il debug
-        navigateRouting(navigate, `activity`, {})
+        saveUserData({ ...userData, type: 1 }, setLoading)
       } else {
         console.error('Failed to fetch user data:', userDataResponse.status);
       }
@@ -164,12 +165,12 @@ const GoogleAuthComponent = () => {
       {/* Snackbar per notifiche */}
       <Snackbar
         open={open}
-        autoHideDuration={6000} // Chiude automaticamente dopo 6 secondi
+        autoHideDuration={6000}
         onClose={handleClose}
-        anchorOrigin={{ vertical: 'top', horizontal: 'center' }} // Posizione del messaggio
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
       >
-        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
-          {errors}
+        <Alert onClose={handleClose} severity={message.typeMessage} sx={{ width: '100%' }}>
+          {message.message}
         </Alert>
       </Snackbar>
 
@@ -216,58 +217,58 @@ const GoogleAuthComponent = () => {
 
                     {/* Seconda riga: Pulsante di login reale */}
                     <Grid item xs={12}>
-                    <GoogleLogin onSuccess={() => login()} onError={logOut} />
+                      <GoogleLogin onSuccess={() => login()} onError={logOut} />
 
                     </Grid>
                   </Grid>
                 </div>
-                </div>
-
-                <div>
-
-
-                  {/* Dialog */}
-                  <Dialog open={openD} onClose={handleCloseD}>
-                    <DialogTitle>Inserisci la tua email parentale </DialogTitle>
-                    <DialogContent>
-                      <TextField
-                        autoFocus
-                        margin="dense"
-                        label="Email"
-                        type="email"
-                        fullWidth
-                        value={email}
-                        onChange={handleEmailChange}
-                      />
-                    </DialogContent>
-                    <DialogActions>
-                      <ButtonMui onClick={handleCloseD} color="secondary">
-                        Annulla
-                      </ButtonMui>
-                      <ButtonMui
-                        onClick={() => handleConfirm(userData)}
-                        color="primary"
-                        disabled={!email} // Disabilita il pulsante se l'email è vuota
-                      >
-                        Conferma
-                      </ButtonMui>
-                    </DialogActions>
-                  </Dialog>
-                </div>
               </div>
-              ) : (
+
               <div>
-                <h1>{title}</h1>
-                <Routes>
-                  <Route path="/activity" element={<Activity user={user} />} />
-                  <Route path="/about" element={<About user={user} />} />
-                  <Route path="/points" element={<Points user={user} />} />
-                  <Route path="/operative" element={<Operative user={user} />} />
-                  <Route path="/family" element={<Family user={user} />} />
-                </Routes>
+
+
+                {/* Dialog */}
+                <Dialog open={openD} onClose={handleCloseD}>
+                  <DialogTitle>Inserisci la tua email parentale </DialogTitle>
+                  <DialogContent>
+                    <TextField
+                      autoFocus
+                      margin="dense"
+                      label="Email"
+                      type="email"
+                      fullWidth
+                      value={email}
+                      onChange={handleEmailChange}
+                    />
+                  </DialogContent>
+                  <DialogActions>
+                    <ButtonMui onClick={handleCloseD} color="secondary">
+                      Annulla
+                    </ButtonMui>
+                    <ButtonMui
+                      onClick={() => handleConfirm(userData)}
+                      color="primary"
+                      disabled={!email} // Disabilita il pulsante se l'email è vuota
+                    >
+                      Conferma
+                    </ButtonMui>
+                  </DialogActions>
+                </Dialog>
               </div>
-          )}
             </div>
+          ) : (
+            <div>
+              <h1>{title}</h1>
+              <Routes>
+                <Route path="/activity" element={<Activity user={user} />} />
+                <Route path="/about" element={<About user={user} />} />
+                <Route path="/points" element={<Points user={user} />} />
+                <Route path="/operative" element={<Operative user={user} />} />
+                <Route path="/family" element={<Family user={user} />} />
+              </Routes>
+            </div>
+          )}
+        </div>
       </GoogleOAuthProvider>
 
       {/* Mostra il loader se loading è true */}
@@ -297,8 +298,7 @@ export const sezioniMenuIniziale = (user: UserI): MenuLaterale[][] => {
   } else {
     return [
       [
-        { funzione: null, testo: 'Activity' },
-        { funzione: null, testo: 'About' },
+        { funzione: null, testo: 'Activity' },  
         { funzione: null, testo: 'Points' },
         { funzione: null, testo: 'Operative' },
       ]
