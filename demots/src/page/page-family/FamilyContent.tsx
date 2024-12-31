607837a3fef7735af55b854e328c5f7fee6f7e7f
Matt7046 <matteo.santangelo@colorsdev.tech>
2024-12-31 06:34:19 +0100

    corezione stile sezione family

--- demots/src/page/page-family/FamilyContent.tsx
+++ demots/src/page/page-family/FamilyContent.tsx
@@ -1,6 +1,6 @@
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
-import { Box, Button as ButtonMui, Grid } from "@mui/material";
+import { Box, FormControl, Grid, IconButton, Input, InputAdornment, InputLabel } from "@mui/material";
 import TextField from '@mui/material/TextField';
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -38,6 +38,17 @@ const FamilyContent: React.FC<FamilyContentProps> = ({
   const [newPoints, setNewPoints] = useState<number>(100);
   const [points, setPoints] = useState<number>(0);
   const [isPlusIcon, setIsPlusIcon] = useState(true);
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  const handleMouseUpPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
 
   const toggleIcon = () => {
     setIsPlusIcon((prev) => !prev);
@@ -48,9 +59,9 @@ const FamilyContent: React.FC<FamilyContentProps> = ({
       setIsVertical(window.innerHeight > window.innerWidth);
     };
     window.addEventListener("resize", handleResize);
-    const emailFind = user.emailFamily ? user.emailFamily: user.email;
+    const emailFind = user.emailFamily ? user.emailFamily : user.email;
 
-    findByEmail({...user, email : emailFind}, (message: any) => showMessage(setOpen, setMessage, message)).then((response: ResponseI) => {
+    findByEmail({ ...user, email: emailFind }, (message: any) => showMessage(setOpen, setMessage, message)).then((response: ResponseI) => {
       if (response) {
         if (response.status === HttpStatus.OK) {
           setPoints(response.testo.points);
@@ -102,100 +113,100 @@ const FamilyContent: React.FC<FamilyContentProps> = ({
     // Pulisci il listener al dismount
     return () => window.removeEventListener("resize", handleResize);
   }, []);
-  
+
   const salvaRecord = (userData: any): Promise<any> => {
     //  const utente = { email: userData.email, type: userData.type }
-    const pointsWithPlus =  isPlusIcon ? newPoints : - newPoints;
-    return savePointsByTypeStandard({...userData, points: points, usePoints: pointsWithPlus}, (message: any) => showMessage(setOpen, setMessage, message)).then((x) => {
+    const pointsWithPlus = isPlusIcon ? newPoints : - newPoints;
+    return savePointsByTypeStandard({ ...userData, points: points, usePoints: pointsWithPlus }, (message: any) => showMessage(setOpen, setMessage, message)).then((x) => {
       console.log('User Data:', x); // Logga i dati utente per il debug
       setPoints(x.testo.points)
-     // navigateRouting(navigate, `activity`, {})
+      // navigateRouting(navigate, `activity`, {})
     })
   }
-return (
-  <>
-    <div className="row">
-      <Box sx={{ padding: 2 }}>
+  return (
+    <>
+      <div className="row">
+        <Box sx={{ padding: 2 }}>
 
-        <div id="text-box-email-family">
-          <TextField
-            id="emailFamily"
-            label={labelFamily.emailFamily}
-            variant="standard"
-            value={user.emailFamily} // Collega il valore allo stato
-            onChange={handleChangeEmailFamily} // Aggiorna lo stato quando cambia
-            fullWidth
-            disabled={true}
-          />
-        </div>
+          <div id="text-box-email-family">
+            <TextField
+              id="emailFamily"
+              label={labelFamily.emailFamily}
+              variant="standard"
+              value={user.emailFamily} // Collega il valore allo stato
+              onChange={handleChangeEmailFamily} // Aggiorna lo stato quando cambia
+              fullWidth
+              disabled={true}
+            />
+          </div>
 
-        <div id="text-box-email" style={{ marginTop: '16px' }}>
-          <TextField
-            id="email"
-            label={labelFamily.email}
-            variant="standard"
-            value={user.email} // Collega il valore allo stato
-            onChange={handleChangeEmail} // Aggiorna lo stato quando cambia
-            fullWidth
-            disabled={true}
-            multiline
-            InputLabelProps={{
-              style: {
-                whiteSpace: 'normal', // Permette al testo di andare a capo
-                wordWrap: 'break-word', // Interrompe le parole lunghe
-              },
-            }}
-          />
-        </div>
-        <div id="text-box-points">
-          <TextField
-            id="points"
-            label={labelFamily.points}
-            variant="standard"
-            value={points} // Collega il valore allo stato
-            onChange={handleChangePoints} // Aggiorna lo stato quando cambia
-            fullWidth
-            disabled={true}
-            type="number"
-          />
-        </div>
-        <ButtonMui
-  variant="contained"
-  color="primary"
-  onClick={toggleIcon}
-  style={{
-    marginTop: '10px',
-    width: '40px', // Imposta una larghezza fissa più piccola
-    height: '40px', // Mantieni l'altezza invariata
-    minWidth: 'unset', // Impedisce al pulsante di espandersi oltre la larghezza definita
-  }}
->
-  {isPlusIcon ? <AddIcon /> : <RemoveIcon />}
-</ButtonMui>
-        <div id="text-box-use-points">
-          <TextField
-            id="UsePoints"
-            label={labelFamily.newPoints}
-            variant="standard"
-            value={newPoints} // Collega il valore allo stato
-            onChange={handleChangeNewPoints} // Aggiorna lo stato quando cambia
-            fullWidth
-            type="number"
-            style={{
-              marginTop: '10px'          
-            }}
-          />
-        </div>
-      </Box>
-      {/* Pulsanti */}
-      <Grid container justifyContent="flex-end" spacing={2}>
-        <Grid item>
-          <Button pulsanti={[pulsanteBlue]} />
+          <div id="text-box-email" style={{ marginTop: '16px' }}>
+            <TextField
+              id="email"
+              label={labelFamily.email}
+              variant="standard"
+              value={user.email} // Collega il valore allo stato
+              onChange={handleChangeEmail} // Aggiorna lo stato quando cambia
+              fullWidth
+              disabled={true}
+              multiline
+              InputLabelProps={{
+                style: {
+                  whiteSpace: 'normal', // Permette al testo di andare a capo
+                  wordWrap: 'break-word', // Interrompe le parole lunghe
+                },
+              }}
+            />
+          </div>
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
+            <InputLabel htmlFor="filled-points">{labelFamily.points}</InputLabel>
+            <Input
+              id="filled-adornment-points"
+              value={points} // Collega il valore allo stato
+              onChange={handleChangePoints} // Aggiorna lo stato quando cambia
+
+              disabled={true}
+            />
+          </FormControl>
+
+
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
+            <InputLabel htmlFor="filled-adornment-points">New Points</InputLabel>
+            <Input
+              id="filled-adornment-points"
+              value={newPoints} // Collega il valore allo stato
+              onChange={handleChangeNewPoints} // Aggiorna lo stato quando cambia
+              type={'number'}
+              startAdornment={
+                <InputAdornment position="start">
+                  <IconButton
+                    aria-label={
+                      showPassword ? 'hide the password' : 'display the password'
+                    }
+                    onClick={toggleIcon}
+                    onMouseDown={handleMouseDownPassword}
+                    onMouseUp={handleMouseUpPassword}
+                    edge="end"
+                  >
+                    {isPlusIcon ? <AddIcon /> : <RemoveIcon />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+
+
+
+        </Box>
+        {/* Pulsanti */}
+        <Grid container justifyContent="flex-end" spacing={2}>
+          <Grid item>
+            <Button pulsanti={[pulsanteBlue]} />
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
-  </>
-);
+      </div>
+    </>
+  );
 }
 
 export default FamilyContent;
