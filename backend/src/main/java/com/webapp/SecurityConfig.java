620142a7b404373776d20c78ce8975820ff43651
Matt7046 <matteo.santangelo@colorsdev.tech>
2025-01-03 08:31:49 +0100

    configurazione locale

--- backend/src/main/java/com/webapp/SecurityConfig.java
+++ backend/src/main/java/com/webapp/SecurityConfig.java
@@ -1,10 +1,6 @@
 package com.webapp;
 
-import java.util.List;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.config.EnableWebFlux;
@@ -16,8 +12,8 @@ public class SecurityConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://webapp-tn6q.onrender.com")  // Aggiungi http:// per localhost
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedOrigins("http://localhost:3000", "https://webapp-tn6q.onrender.com")  // Aggiungi http:// per localhost
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);  // Aggiungi questo se invii cookie o header di autenticazione
     }
