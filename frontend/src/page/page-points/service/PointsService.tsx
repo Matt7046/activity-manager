4af6eb9bdcca88fbc931ddb8db9558ee401a43ea
Matt7046 <matteo.santangelo@colorsdev.tech>
2025-01-06 12:00:08 +0100

    eliminato servizio deprecato

--- frontend/src/page/page-points/service/PointsService.tsx
+++ frontend/src/page/page-points/service/PointsService.tsx
@@ -28,18 +28,6 @@ export const getUserType = async (user: any, funzioneMessage?: (message?: TypeMe
 };
 
 
-export const savePointsByTypeStandard = async (user: any, funzioneMessage?: (message?: TypeMessage) => void, setLoading?: (loading: boolean) => void,
-  showSuccess?: boolean) => {
-  try {
-    const path = PATH_POINTS + `/dati/standard`;
-    showSuccess = true; 
-    const data = await postData(path, user, setLoading, funzioneMessage, showSuccess); // Endpoint dell'API
-    console.log('Dati ricevuti:', data);
-    return data;
-  } catch (error) {
-    console.error('Errore durante il recupero dei dati:', error);
-  }
-};
 
 
 
