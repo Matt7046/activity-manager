14f0855a554e576029f51509d1eafcbb7d200afe
Matt7046 <matteo.santangelo@colorsdev.tech>
2025-01-09 13:41:47 +0100

    merge main in production

--- backend/src/main/java/com/activityManager/SecurityConfig.java
+++ backend/src/main/java/com/activityManager/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000", "https://webapp-tn6q.onrender.com")  // Aggiungi http:// per localhost
+        .allowedOrigins("https://webapp-tn6q.onrender.com")  // Aggiungi http:// per localhost
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // Aggiungi questo se invii cookie o header di autenticazione
