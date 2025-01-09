2c0f632cde219476cf746ab509e40653ccca9a2a
Matt7046 <matteo.santangelo@colorsdev.tech>
2025-01-09 13:39:41 +0100

    gestione beaver

---
+++ backend/src/main/java/com/activityManager/trasversali/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.activityManager.trasversali;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // Genera getter, setter, toString, equals e hashCode
+@NoArgsConstructor // Genera un costruttore senza argomenti
+@AllArgsConstructor // Genera un costruttore con tutti i campi
+public class LoginRequest {
+    private String email;   
+
+    // Getter e Setter
+}
--- frontend/src/page/page-register/store/RegisterStore.tsx
+++ frontend/src/page/page-register/store/RegisterStore.tsx
@@ -1,4 +1,3 @@
-import { observer } from 'mobx-react';
 import BaseStore from '../../../general/BaseStore';
 
 export class RegisterStore extends BaseStore {
@@ -14,8 +13,8 @@ export class RegisterStore extends BaseStore {
     return this;
   }
 }
-const activityStore = new RegisterStore();
-export default activityStore;
+const registerStore = new RegisterStore();
+export default registerStore;
 
 
 
