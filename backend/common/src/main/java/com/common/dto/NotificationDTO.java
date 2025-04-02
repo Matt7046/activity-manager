04d6943838ff408405104016a6e85f8e3d4ba709
Matt7046 <matteo.santangelo@colorsdev.tech>
2025-04-02 10:15:43 +0200

    inserito sistema di notifiche all'aggiunta dei points da family

---
+++ backend/common/src/main/java/com/common/dto/NotificationDTO.java
@@ -0,0 +1,18 @@
+package com.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // Genera getter, setter, toString, equals e hashCode
+@NoArgsConstructor // Genera un costruttore senza argomenti
+@AllArgsConstructor // Genera un costruttore con tutti i campi
+public class FamilyNotificationDTO implements InterfaceDTO {
+
+    private String _id; // Identificatore unico
+    private String subTesto;
+    private String nome;
+    private String logAttivita;
+    private Long points;
+    private String email;
+}
