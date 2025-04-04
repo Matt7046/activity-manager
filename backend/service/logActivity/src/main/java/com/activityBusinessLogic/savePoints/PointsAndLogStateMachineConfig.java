a39e8e3ae135a48fbd343a1934eacb19a8f21e59
Matt7046 <matteo.santangelo@colorsdev.tech>
2025-04-04 17:32:40 +0200

    inserimento state machine per transazioni savePoints

---
+++ backend/service/logActivity/src/main/java/com/activityBusinessLogic/savePoints/PointsAndLogStateMachineConfig.java
@@ -0,0 +1,43 @@
+package com.activityBusinessLogic.savePoints;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachine;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+
+@Configuration
+@EnableStateMachineFactory
+public class PointsAndLogStateMachineConfig extends EnumStateMachineConfigurerAdapter<State, Event> {
+    @Override
+    public void configure(StateMachineStateConfigurer<State, Event> states) throws Exception {
+        states
+                .withStates()
+                .initial(State.START)
+                .state(State.SAVE_POINTS)
+                .state(State.SAVE_LOG)
+                .state(State.COMPENSATE)
+                .end(State.SUCCESS)
+                .end(State.FAILED);
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<State, Event> transitions) throws Exception {
+        transitions
+                .withExternal()
+                .source(State.START).target(State.SAVE_POINTS).event(Event.PROCESS_POINTS)
+                .and()
+                .withExternal()
+                .source(State.SAVE_POINTS).target(State.SAVE_LOG).event(Event.LOG_SAVED)
+                .and()
+                .withExternal()
+                .source(State.SAVE_LOG).target(State.SUCCESS).event(Event.SUCCESS)
+                .and()
+                .withExternal()
+                .source(State.SAVE_POINTS).target(State.COMPENSATE).event(Event.ERROR)
+                .and()
+                .withExternal()
+                .source(State.COMPENSATE).target(State.FAILED).event(Event.COMPENSATED);
+    }
+}
