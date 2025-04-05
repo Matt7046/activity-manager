41917fefeb354cdf0d30c844a1d5f40813ac96dc
Matt7046 <matteo.santangelo@colorsdev.tech>
2025-04-05 10:18:24 +0200

    struttura per multioperazioni su about con possibilità di gestire rollback

--- backend/service/about/src/main/java/com/activityService/PointsService.java
+++
@@ -1,5 +0,0 @@
-package com.activityService;
-
-public class PointsService {
-
-}
---
+++ backend/service/about/src/main/java/com/deleteActivityAbout/Event.java
@@ -0,0 +1,7 @@
+package com.deleteActivityAbout;
+
+
+
+public enum Event {
+    PROCESS_ACTIVITY, SUCCESS, ERROR, COMPENSATED;
+}
