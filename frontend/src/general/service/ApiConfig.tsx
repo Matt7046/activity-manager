b8a302f72f7eeee8f16c2d1bca5bfd88db7472c6
Matt7046 <matteo.santangelo@colorsdev.tech>
2025-04-05 22:31:57 +0200

    microservizi scalabili

--- frontend/src/general/service/ApiConfig.tsx
+++ frontend/src/general/service/ApiConfig.tsx
@@ -18,28 +18,28 @@ export interface SingleServiceConfig {
 // Configurazione completa per tutti i servizi
 const apiConfig: Record<ServiceName, SingleServiceConfig> = {
   registerService: {
-    baseURL: process.env.REACT_APP_REGISTER_SERVICE_URL || 'http://localhost:8081/api',
+    baseURL: process.env.REACT_APP_REGISTER_SERVICE_URL || 'http://localhost/api',
   },
   aboutService: {
-    baseURL: process.env.REACT_APP_ABOUT_SERVICE_URL || 'http://localhost:8082/api',
+    baseURL: process.env.REACT_APP_ABOUT_SERVICE_URL || 'http://localhost/api',
   },
   pointsService: {
-    baseURL: process.env.REACT_APP_POINTS_SERVICE_URL || 'http://localhost:8083/api',
+    baseURL: process.env.REACT_APP_POINTS_SERVICE_URL || 'http://localhost/api',
   },
   activityService: {
-    baseURL: process.env.REACT_APP_ACTIVITY_SERVICE_URL || 'http://localhost:8084/api',
+    baseURL: process.env.REACT_APP_ACTIVITY_SERVICE_URL || 'http://localhost/api',
   },
   logActivityService: {
-    baseURL: process.env.REACT_APP_LOG_ACTIVITY_SERVICE_URL || 'http://localhost:8085/api',
+    baseURL: process.env.REACT_APP_LOG_ACTIVITY_SERVICE_URL || 'http://localhost/api',
   },
   authService: {
-    baseURL: process.env.REACT_APP_LOG_AUTH_SERVICE_URL || 'http://localhost:8086/api',
+    baseURL: process.env.REACT_APP_LOG_AUTH_SERVICE_URL || 'http://localhost/api',
   },
   familyService: {
-    baseURL: process.env.REACT_APP_LOG_FAMILY_SERVICE_URL || 'http://localhost:8087/api',
+    baseURL: process.env.REACT_APP_LOG_FAMILY_SERVICE_URL || 'http://localhost/api',
   },
   notificationService: {
-    baseURL: process.env.REACT_APP_LOG_FAMILY_SERVICE_URL || 'http://localhost:8088/api',
+    baseURL: process.env.REACT_APP_LOG_FAMILY_SERVICE_URL || 'http://localhost/api',
   },
 };
 
@@ -48,3 +48,4 @@ export default apiConfig;
 
 
 
+7
