993c0c4d1d9077bfa25563dd7c2b51c1164693df
Matt7046 <matteo.santangelo@colorsdev.tech>
2025-04-11 13:11:11 +0200

    preparazione introduzione css

---
+++ frontend/src/components/ms-dialog-email/DialogEmail.tsx
@@ -0,0 +1,64 @@
+import { Button as ButtonMui, Dialog, DialogActions, DialogContent, DialogTitle, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import React from 'react';
+import './DialogEmail.css'; // Importa il file CSS
+
+interface EmailDialogProps {
+  openD: boolean;
+  handleCloseD: () => void;
+  emailOptions: string[];
+  handleEmailChange: (event: SelectChangeEvent) => void;
+  handleConfirm: (simulated: any, emailLogin: string, emailUserCurrent: string) => void;
+  email: string;
+  simulated: any;
+  emailLogin: string;
+  emailUserCurrent: string;
+}
+
+const DialogEmail: React.FC<EmailDialogProps> = ({
+  openD,
+  handleCloseD,
+  emailOptions,
+  handleEmailChange,
+  handleConfirm,
+  email,
+  simulated,
+  emailLogin,
+  emailUserCurrent
+}) => {
+  return (
+    <Dialog open={openD} onClose={handleCloseD} className="dialog-container">
+      <DialogTitle className="dialog-title">Inserisci email del figlio</DialogTitle>
+      <DialogContent>
+        <InputLabel>Email</InputLabel>
+        <Select
+          value={email}
+          onChange={(event: any) => handleEmailChange(event)}
+          label="Email"
+          autoWidth
+          className="dialog-select" // Applicando la classe CSS
+        >
+          {emailOptions?.map((option) => (
+            <MenuItem key={option} value={option}>
+              {option}
+            </MenuItem>
+          ))}
+        </Select>
+      </DialogContent>
+      <DialogActions className="dialog-actions">
+        <ButtonMui onClick={handleCloseD} className="button-cancel" color="secondary">
+          Annulla
+        </ButtonMui>
+        <ButtonMui
+          onClick={() => handleConfirm(simulated, emailLogin, emailUserCurrent)}
+          className="button-confirm"
+          color="primary"
+          disabled={!email} // Disabilita il pulsante se l'email è vuota
+        >
+          Conferma
+        </ButtonMui>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default DialogEmail;
