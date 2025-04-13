bd68b6a0121f96ae43b50e719298597afea58d47
Matt7046 <matteo.santangelo@colorsdev.tech>
2025-04-13 19:25:00 +0200

    change import

--- backend/common/src/main/java/com/common/data/user/UserPoint.java
+++ backend/common/src/main/java/com/common/data/user/UserPoint.java
@@ -8,7 +8,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-import com.common.authDTO.PointsUser;
+import com.common.dto.auth.PointsUser;
 
 @Document(collection = "userpoint") // Specifica la collezione nel database
 @Data // Genera getter, setter, toString, equals e hashCode
