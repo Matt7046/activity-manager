b9034745fc0e6e163f0fb01dc898f9b75009a583
Matt7046 <matteo.santangelo@colorsdev.tech>
2025-04-13 19:25:09 +0200

    change import

--- backend/common/src/main/java/com/common/dto/family/LogFamilyDTO.java
+++ backend/common/src/main/java/com/common/dto/family/LogFamilyDTO.java
@@ -1,6 +1,8 @@
-package com.common.dto;
+package com.common.dto.family;
 
-import com.common.data.OperationTypeLogFamily;
+import com.common.data.family.OperationTypeLogFamily;
+import com.common.dto.structure.InterfaceDTO;
+import com.common.dto.user.UserPointDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,7 @@ import java.util.Date;
 @Data // Genera getter, setter, toString, equals e hashCode
 @NoArgsConstructor // Genera un costruttore senza argomenti
 @AllArgsConstructor // Genera un costruttore con tutti i campi
-public class LogFamilyDTO implements  InterfaceDTO {
+public class LogFamilyDTO implements InterfaceDTO {
     private String _id;
     private String performedByEmail;
     private String receivedByEmail;
