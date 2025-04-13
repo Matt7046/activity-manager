59ca6b665d5b9f1fbbcc9f2a68025b062c4a4f63
Matt7046 <matteo.santangelo@colorsdev.tech>
2025-04-13 10:47:46 +0200

    refactoring + aggiunta card logFamily

--- backend/service/family/src/main/java/com/familyService/service/FamilyWebService.java
+++ backend/service/family/src/main/java/com/familyService/service/FamilyWebService.java
@@ -1,24 +1,37 @@
 package com.familyService.service;
 
-import com.familyService.processor.FamilySavePointsProcessor;
-import com.common.dto.LogFamilyDTO;
+import com.common.authDTO.PointsUser;
 import com.common.dto.UserPointDTO;
 import com.common.dto.ResponseDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import java.util.Optional;
 
 @Service
 public class FamilyWebService {
 
     @Autowired
-    private FamilySavePointsProcessor familySavePointsProcessor;
+    @Qualifier("webClientPoint")
+    private WebClient webClientPoint;
 
-    public Mono<ResponseDTO> savePointsByFamily(UserPointDTO userPointDTO) {
-        return familySavePointsProcessor.savePointsByFamily(userPointDTO);
+    public Mono<Optional<PointsUser>> savePointsByFamily(UserPointDTO userPointDTO) {
+         return webClientPoint.post()
+                .uri("/api/point/dati/standard")
+                .bodyValue(userPointDTO)
+                .retrieve()
+                .bodyToMono(ResponseDTO.class)
+                .flatMap(responseDTO -> Mono.fromCallable(() -> {
+                    UserPointDTO subDTO = new ObjectMapper().convertValue(responseDTO.getJsonText(), UserPointDTO.class);
+                    return subDTO.getPoints().stream()
+                            .filter(point -> userPointDTO.getEmailFamily().equals(point.getEmail()))
+                            .findFirst();
+                }).subscribeOn(Schedulers.boundedElastic()));
     }
 
-    public Mono<ResponseDTO> saveLogFamily(LogFamilyDTO logFamilyDTO) {
-        return familySavePointsProcessor.saveLogFamily(logFamilyDTO);
-    }
+
 }
