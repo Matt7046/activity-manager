2137ce9e44f22d812484bafab9ff8dcd90ecf378
Matt7046 <matteo.santangelo@colorsdev.tech>
2025-04-18 14:15:21 +0200

    pulizia codice

--- backend/service/userPoint/src/main/java/com/userPointService/controller/UserPointController.java
+++ backend/service/userPoint/src/main/java/com/userPointService/controller/UserPointController.java
@@ -1,18 +1,9 @@
 package com.userPointService.controller;
 
 import com.common.configurations.encrypt.EncryptDecryptConverter;
-import com.common.data.user.UserPoint;
-import com.common.dto.auth.Point;
-import com.common.dto.user.PointDTO;
-import com.common.dto.user.PointRDTO;
 import com.common.dto.user.UserPointDTO;
 import com.common.dto.structure.ResponseDTO;
-import com.common.dto.user.UserDTO;
-import com.common.structure.status.ActivityHttpStatus;
-import com.common.structure.exception.NotFoundException;
-import com.common.mapper.UserPointMapper;
 import com.userPointService.processor.UserPointProcessor;
-import com.userPointService.service.UserPointService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,9 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-
-import java.util.ArrayList;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("api/userpoint")
