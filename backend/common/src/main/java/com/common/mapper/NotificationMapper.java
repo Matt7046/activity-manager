5119da9a8f854505ef3254a42996826260cf6e94
Matt7046 <matteo.santangelo@colorsdev.tech>
2025-04-21 21:20:30 +0200

    refactoring

--- backend/common/src/main/java/com/common/mapper/NotificationMapper.java
+++ backend/common/src/main/java/com/common/mapper/NotificationMapper.java
@@ -1,28 +1,49 @@
 package com.common.mapper;
 
+import com.common.configurations.encrypt.EncryptDecryptConverter;
 import com.common.data.notification.Notification;
+import com.common.dto.auth.Point;
 import com.common.dto.notification.NotificationDTO;
+import com.common.dto.user.PointDTO;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 
-@Mapper
-public interface NotificationMapper {
-    NotificationMapper INSTANCE = Mappers.getMapper(NotificationMapper.class);
+@Mapper(componentModel = "spring")
+public abstract class NotificationMapper {
 
+    @Autowired
+    EncryptDecryptConverter encryptDecryptConverter;
     // Da Entity a DTO
-    NotificationDTO toDTO(Notification notification);
-
-    List<NotificationDTO> toDTO(List<Notification> notification);
+    public abstract NotificationDTO toDTO(Notification notification);
 
     // Da DTO a Entity
-    Notification fromDTO(NotificationDTO ActivityDto);
+    public abstract Notification fromDTO(NotificationDTO ActivityDto);
 
+    @AfterMapping
+    protected void decryptEmail(Notification notification, @MappingTarget NotificationDTO dto) {
+        if (dto.getUserReceiver() != null) {
+            dto.setUserReceiver(encryptDecryptConverter.decrypt(notification.getUserReceiver()));
+        }
+        if (dto.getUserSender() != null) {
+            dto.setUserSender(encryptDecryptConverter.decrypt(notification.getUserSender()));
+        }
+    }
 
-    List<Notification> fromDTO(List<NotificationDTO> notification);
-
+    @AfterMapping
+    protected void encryptEmail(NotificationDTO dto, @MappingTarget Notification notification) {
+        if (notification.getUserReceiver() != null) {
+            notification.setUserReceiver(encryptDecryptConverter.convert(dto.getUserReceiver()));
+        }
+        if (notification.getUserSender() != null) {
+            notification.setUserSender(encryptDecryptConverter.convert(dto.getUserSender()));
+        }
+    }
 }
 
 
