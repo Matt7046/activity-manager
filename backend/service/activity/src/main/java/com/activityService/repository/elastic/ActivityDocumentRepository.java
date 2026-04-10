93a392fe52811a234764a9527f46819fffb86ac7
Matt7046 <matteo.santangelo@colorsdev.tech>
2026-04-10 15:35:06 +0200

    preparazione tipologia categoria con IA

--- backend/service/activity/src/main/java/com/activityService/repository/elastic/ActivityDocumentRepository.java
+++ backend/service/activity/src/main/java/com/activityService/repository/elastic/ActivityDocumentRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import org.springframework.stereotype.Repository;
 import com.common.data.activity.ActivityDocument;
 
+import java.util.List;
+
 @Repository
 public interface ActivityDocumentRepository extends ElasticsearchRepository<ActivityDocument, String> {
 
     // Qui puoi aggiungere query personalizzate se serve
     // es. List<ActivityDocument> findByUserId(String userId);
+
+    List<ActivityDocument> findByCategoryContaining(String text);
 }
