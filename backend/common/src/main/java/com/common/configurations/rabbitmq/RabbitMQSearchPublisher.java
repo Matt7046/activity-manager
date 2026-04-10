055607f20ff2e722e648b66a70fc6c7dd0772860
Matt7046 <matteo.santangelo@colorsdev.tech>
2026-04-10 17:31:02 +0200

    salvataggio indici in elastic

--- backend/common/src/main/java/com/common/configurations/rabbitmq/RabbitMQSearchPublisher.java
+++ backend/common/src/main/java/com/common/configurations/rabbitmq/RabbitMQSearchPublisher.java
@@ -2,6 +2,8 @@ package com.common.configurations.rabbitmq;
 
 import com.common.data.activity.event.ActivityCreateEvent;
 import com.common.data.activity.event.ActivityEnrichedEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -9,40 +11,53 @@ import org.springframework.stereotype.Component;
 @Component
 public class RabbitMQSearchPublisher implements SearchPublisher {
 
-        private final RabbitTemplate rabbitTemplate;
+    private final RabbitTemplate rabbitTemplate;
 
-        @Value("${rabbitmq.exchange.name.activity.search.exchangeName}")
-        private String exchangeActivitySearch;
+    @Value("${rabbitmq.exchange.name.activity.search.exchangeName}")
+    private String exchangeActivitySearch;
 
-        @Value("${rabbitmq.routingKey.activity.search.create}")
-        private String routingKeySearchCreate;
+    @Value("${rabbitmq.routingKey.activity.search.create}")
+    private String routingKeySearchCreate;
 
-        @Value("${rabbitmq.routingKey.activity.search.enriched}")
-        private String routingKeySearchEnriched;
+    @Value("${rabbitmq.routingKey.activity.search.enriched}")
+    private String routingKeySearchEnriched;
 
-        public RabbitMQSearchPublisher(RabbitTemplate rabbitTemplate) {
-            this.rabbitTemplate = rabbitTemplate;
-        }
+    public RabbitMQSearchPublisher(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
 
-        // ========================
-        // CREATE EVENT (→ AI)
-        // ========================
-        public void publishCreate(ActivityCreateEvent event) {
+    // ========================
+    // CREATE EVENT (→ AI)
+    // ========================
+    public void publishCreate(ActivityCreateEvent event) {
+        try {
+            String jsonMessage = new ObjectMapper().writeValueAsString(event);
             rabbitTemplate.convertAndSend(
                     exchangeActivitySearch,
                     routingKeySearchCreate,
-                    event
+                    jsonMessage
             );
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
 
-        // ========================
-        // ENRICHED EVENT (→ ELASTIC)
-        // ========================
-        public void publishEnriched(ActivityEnrichedEvent event) {
+
+    }
+
+    // ========================
+    // ENRICHED EVENT (→ ELASTIC)
+    // ========================
+    public void publishEnriched(ActivityEnrichedEvent event) {
+        try {
+            String jsonMessage = new ObjectMapper().writeValueAsString(event);
+
             rabbitTemplate.convertAndSend(
                     exchangeActivitySearch,
                     routingKeySearchEnriched,
-                    event
+                    jsonMessage
             );
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
     }
+}
--- backend/service/notification/src/main/java/com/notificationService/rabbitmq/RabbitMQActivityAIConsumer.java
+++ backend/service/notification/src/main/java/com/notificationService/rabbitmq/RabbitMQActivityAIConsumer.java
@@ -3,8 +3,17 @@ package com.notificationService.rabbitmq;
 import com.common.configurations.rabbitmq.SearchPublisher;
 import com.common.data.activity.event.ActivityCreateEvent;
 import com.common.data.activity.event.ActivityEnrichedEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+import java.io.IOException;
 
 @Component
 public class RabbitMQActivityAIConsumer {
@@ -16,23 +25,49 @@ public class RabbitMQActivityAIConsumer {
     }
 
     @RabbitListener(queues = "search.ai.queue")
-    public void handleCreate(ActivityCreateEvent event) {
+    public void handleCreate(String jsonMessage, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws JsonProcessingException {
 
-        // 👉 Qui fai enrichment (anche fake all'inizio)
-        String category = categorize(event);
+        // Simula un ritardo prima dell'ACK per vedere il messaggio su RabbitMQ Management UI
+        try {
+            // Thread.sleep(20000); // 5 secondi
+            receive(jsonMessage, 0);
+            channel.basicAck(tag, false); // Conferma il messaggio SOLO dopo l'elaborazion
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public void receive(String in, int receiver) throws InterruptedException, JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ActivityCreateEvent event = objectMapper.readValue(in, ActivityCreateEvent.class);
+
+        StopWatch watch = new StopWatch();
+        watch.start();
+        doWork(in);
+        watch.stop();
         ActivityEnrichedEvent enriched = new ActivityEnrichedEvent(
                 event._id(),
                 event.subTesto(),
                 event.nome(),
                 event.points(),
                 event.email(),
-                category
+                categorize(event)
         );
 
         // 👉 Pubblica verso Elastic step
         searchPublisher.publishEnriched(enriched);
     }
+
+    private void doWork(String in) throws InterruptedException {
+        for (char ch : in.toCharArray()) {
+            if (ch == '.') {
+                Thread.sleep(1000);
+            }
+        }
+    }
+
 
     private String categorize(ActivityCreateEvent event) {
         // MOCK iniziale
