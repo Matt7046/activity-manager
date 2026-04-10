d1b4717dc28b8d307b2e9468e5f1bd9814ca9032
Matt7046 <matteo.santangelo@colorsdev.tech>
2026-04-10 17:31:02 +0200

    salvataggio indici in elastic

--- backend/service/activity/src/main/java/com/activityService/rabbitmq/RabbitMQActivityElasticConsumer.java
+++ backend/service/activity/src/main/java/com/activityService/rabbitmq/RabbitMQActivityElasticConsumer.java
@@ -3,8 +3,16 @@ package com.activityService.rabbitmq;
 import com.activityService.repository.elastic.ActivityDocumentRepository;
 import com.common.data.activity.ActivityDocument;
 import com.common.data.activity.event.ActivityEnrichedEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.amqp.support.AmqpHeaders;
+
+import java.io.IOException;
 
 @Component
 public class RabbitMQActivityElasticConsumer {
@@ -16,6 +24,49 @@ public class RabbitMQActivityElasticConsumer {
     }
 
     @RabbitListener(queues = "search.elastic.queue")
+    public void handleEnriched(String jsonMessage, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws JsonProcessingException {
+
+        // Simula un ritardo prima dell'ACK per vedere il messaggio su RabbitMQ Management UI
+        try {
+            // Thread.sleep(20000); // 5 secondi
+            receive(jsonMessage, 0);
+            channel.basicAck(tag, false); // Conferma il messaggio SOLO dopo l'elaborazion
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void receive(String in, int receiver) throws InterruptedException, JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ActivityEnrichedEvent event = objectMapper.readValue(in, ActivityEnrichedEvent.class);
+
+        StopWatch watch = new StopWatch();
+        watch.start();
+        doWork(in);
+        watch.stop();
+        ActivityDocument doc = new ActivityDocument();
+        doc.setIdentificativo(event._id());
+        doc.setNome(event.nome());
+        doc.setSubTesto(event.subTesto());
+        doc.setPoints(event.points());
+        doc.setEmail(event.email());
+        doc.setCategory(event.category());
+
+        repository.save(doc);
+
+    }
+
+    private void doWork(String in) throws InterruptedException {
+        for (char ch : in.toCharArray()) {
+            if (ch == '.') {
+                Thread.sleep(1000);
+            }
+        }
+    }
+
+
     public void handleEnriched(ActivityEnrichedEvent event) {
 
         ActivityDocument doc = new ActivityDocument();
