e94744a589ff39fe2ae3222b2536d4efbf474db0
Matt7046 <matteo.santangelo@colorsdev.tech>
2026-04-23 12:09:21 +0200

    creazione sezione gamification

---
+++ frontend/src/page/page-gamification/Gamification.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useUser } from '../../App';
+import { TypeUser } from '../../general/structure/Constant';
+import { getMenuLaterale } from '../../general/structure/Utils';
+import PageLayout, { TypeMessage } from '../page-layout/PageLayout';
+import GamificationContent from './GamificationContent';
+
+
+const Gamification: React.FC<{}> = ({ }) => {
+  const { user, setUser } = useUser();
+  const subTitle = user.type === TypeUser.FAMILY ? ' (tutorato)' : ''
+  const [title, setTitle] = useState<string>("Sezione sfide e premi" + subTitle);
+  const navigate = useNavigate(); // Ottieni la funzione di navigazione
+  const menuLaterale = getMenuLaterale(navigate, user);
+  const [open, setOpen] = useState(false); // Controlla la visibilità del messaggio
+  const [isVertical, setIsVertical] = useState<boolean>(window.innerHeight > window.innerWidth);
+  const [paddingType, setPaddingType] = useState<number>(isVertical ? 0 : 5);
+  const [message, setMessage] = React.useState<TypeMessage>({}); // Lo stato è un array di stringhe
+
+
+  useEffect(() => {
+
+    const handleResize = () => {
+      setIsVertical(window.innerHeight > window.innerWidth);
+      setPaddingType(window.innerHeight > window.innerWidth ? 0 : 5);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Pulisci il listener al dismount
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <PageLayout
+        title={title}
+        menuLaterale={menuLaterale}
+        alertConfig={{open,setOpen,message,setMessage}}
+        isVertical={isVertical}
+        handleClose={handleClose}
+        navigate={useNavigate()}
+      >
+        <GamificationContent
+          user={user}
+          alertConfig={{open,setOpen,message,setMessage}}
+          isVertical={isVertical}
+        />
+      </PageLayout>
+      <div>
+        {/* Contenuto aggiuntivo, se necessario */}
+      </div>
+    </>
+  );
+};
+
+export default Gamification;
+
+
+
+
+
+
---
+++ frontend/src/page/page-gamification/store/GamificationStore.tsx
@@ -0,0 +1,48 @@
+import { makeObservable, observable } from 'mobx';
+import { VideoI } from '../../../components/ms-video-grid/MsVideoGrid';
+import BaseStore from '../../../general/structure/BaseStore';
+
+export class GamificationStore extends BaseStore {
+
+  @observable video = [] as VideoI[];
+  @observable points = 0 as number;
+  @observable minutes = 0 as number;
+
+  constructor() {
+    super(); // Inizializza la classe base
+    makeObservable(this);
+
+  }
+
+  getStore() {
+    return this;
+  }
+
+  addPoints(value: number) {
+    this.points += value;
+    this.minutes = 0;
+  }
+
+  getPoints() {
+    return this.points;
+  }
+
+  getMinutes() {
+    return this.minutes;
+  }
+  
+  setVideo(video: VideoI[]) {
+    this.video = video;
+  }
+
+  setMinutes(minutes: number) {
+    this.minutes = minutes;
+  }
+}
+const gamificationStore = new GamificationStore();
+export default gamificationStore;
+
+
+
+
+
