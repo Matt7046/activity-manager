47149b68dbdcd1cc55c6f06fb07a257bb8c83312
Matt7046 <matteo.santangelo@colorsdev.tech>
2026-04-23 17:23:58 +0200

    correzione apiconfig

--- frontend/src/general/structure/ApiConfig.tsx
+++ frontend/src/general/structure/ApiConfig.tsx
@@ -4,11 +4,7 @@ export interface SingleServiceConfig {
 
 // Configurazione completa per tutti i servizi
 const apiConfig: SingleServiceConfig = {
-<<<<<<< HEAD
   baseURL: process.env.REACT_APP_SERVICE_URL || '/api',
-=======
-  baseURL: process.env.REACT_APP_SERVICE_URL || 'http://173.212.220.20/api',
->>>>>>> d76af0e0a8c1fae4800ebd0e733bb5310558782a
 };
 
 export default apiConfig;
