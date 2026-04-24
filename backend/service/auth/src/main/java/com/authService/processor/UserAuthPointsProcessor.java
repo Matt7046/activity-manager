0b3ea9dd8c0eed788317a066b2bf0d8c4748017d
Matt7046 <matteo.santangelo@colorsdev.tech>
2026-04-24 17:04:29 +0200

    commit funzionalità token

--- backend/service/auth/src/main/java/com/authService/processor/UserAuthPointsProcessor.java
+++ backend/service/auth/src/main/java/com/authService/processor/UserAuthPointsProcessor.java
@@ -1,15 +1,18 @@
 package com.authService.processor;
 
 import com.authService.service.AuthService;
+import com.common.configurations.structure.JwtUtil;
 import com.common.dto.auth.LoginRequest;
 import com.common.dto.auth.LoginResponse;
 import com.common.dto.structure.ResponseDTO;
+import com.common.dto.user.UserPointDTO;
 import com.common.structure.status.ActivityHttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.stereotype.Component;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
@@ -17,17 +20,35 @@ import java.util.ArrayList;
 @Component
 public class UserAuthPointsProcessor {
 
+    @Autowired
+    JwtUtil jwtUtil;
 
     @Autowired
     AuthService authService;
+
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Value("${app.secret.crypt.user.name}")
+    private String secretName;
+
+    @Value("${app.secret.crypt.user.key}")
+    private String secretKey;
+
     public Mono<ResponseDTO> getToken(LoginRequest loginRequest) {
         Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
-        LoginResponse login =  authService.getToken(authentication);
-        ResponseDTO response = new ResponseDTO(login, ActivityHttpStatus.OK.value(), new ArrayList<>());
-        return Mono.just(response);
+                new UsernamePasswordAuthenticationToken(secretName, secretKey));
+        String token = jwtUtil.generateToken(authentication.getName());
+        return authService.checkUserExists(loginRequest)
+                .flatMap(exists -> {
+                    if (exists) {
+                        return Mono.just(new ResponseDTO(new LoginResponse(token), ActivityHttpStatus.OK.value(),
+                                new ArrayList<>()));
+                    } else {
+                        // Se non esiste, ritorno una risposta vuota o un errore
+                        return Mono.just(new ResponseDTO(new LoginResponse(""), ActivityHttpStatus.INTERNAL_SERVER_ERROR.value(),
+                                new ArrayList<>()));
+                    }
+                });
     }
 }
