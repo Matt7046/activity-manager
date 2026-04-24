c75fed7469de134ce31ff1dc3136b4684a5e76af
Matt7046 <matteo.santangelo@colorsdev.tech>
2026-04-24 08:36:56 +0200

    correzione path abilitati

--- backend/service/activity/src/main/java/com/activityService/controller/ActivityController.java
+++ backend/service/activity/src/main/java/com/activityService/controller/ActivityController.java
@@ -2,26 +2,12 @@ package com.activityService.controller;
 
 import com.activityService.processor.ActivityCommandProcessor;
 import com.activityService.processor.ActivityQueryProcessor;
-import com.activityService.service.ActivityService;
 import com.common.dto.user.UserPointDTO;
-import com.common.structure.status.ActivityHttpStatus;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.common.configurations.encrypt.EncryptDecryptConverter;
-import com.common.data.activity.Activity;
 import com.common.dto.activity.ActivityDTO;
 import com.common.dto.structure.ResponseDTO;
-
-import com.common.structure.exception.NotFoundException;
-import com.common.mapper.ActivityMapper;
-
 import reactor.core.publisher.Mono;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
