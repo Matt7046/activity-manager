c5d9a0f01d37666c3a1712c552236aea020b862c
Matt7046 <matteo.santangelo@colorsdev.tech>
2026-04-24 18:52:50 +0200

    change presentation

--- frontend/src/components/ms-presentation/Presentation.tsx
+++ frontend/src/components/ms-presentation/Presentation.tsx
@@ -2,6 +2,8 @@ import { Trans } from "@lingui/react";
 import ReactIcon from '@mui/icons-material/Code'; // Potrebbe non esserci un'icona specifica, usa un generico
 import JavaIcon from '@mui/icons-material/Coffee'; // Esempio
 import SpringBootIcon from '@mui/icons-material/PlayCircleFilled'; // Esempio
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import SearchIcon from '@mui/icons-material/Search';
 import MongoDBIcon from '@mui/icons-material/Storage'; // Esempio di icona Material UI
 import { Box, Button, Grid, IconButton, Typography } from '@mui/material';
 import { useState } from 'react';
@@ -29,6 +31,8 @@ const Presentation: React.FC<PresentationProps> = ({ }) => {
     { name: 'React', icon: <ReactIcon fontSize="large" sx={{ color: 'red' }} /> },
     { name: 'Java', icon: <JavaIcon fontSize="large" sx={{ color: 'red' }} /> },
     { name: 'Spring Boot', icon: <SpringBootIcon fontSize="large" sx={{ color: 'red' }} /> },
+    { name: 'ElasticSearch', icon: <SearchIcon fontSize="large" sx={{ color: 'red' }} /> },
+    { name: 'GPT (AI)', icon: <PsychologyIcon fontSize="large" sx={{ color: 'red' }} /> },
     // Aggiungi altre tecnologie con le relative icone
   ];
 
@@ -37,7 +41,7 @@ const Presentation: React.FC<PresentationProps> = ({ }) => {
     <>
       <BannerOpenSource />
       <Box
-        className="welcome-container"      
+        className="welcome-container"
       >
         <Typography
           variant="h4"
@@ -45,7 +49,7 @@ const Presentation: React.FC<PresentationProps> = ({ }) => {
           htmlFor="enter-button"
           gutterBottom
           fontWeight="bold"
-        
+
         >
           {testoEntry}
         </Typography>
@@ -69,50 +73,38 @@ const Presentation: React.FC<PresentationProps> = ({ }) => {
           color="primary"
           size="large"
           onClick={() => navigateRouting(navigate, SectionName.HOME, {})}
-          sx={{
-            backgroundColor: '#4a5568', // Bottone grigio scuro
-            color: '#f7fafc',
-            padding: (theme) => theme.spacing(2, 4),
-            borderRadius: '5px',
-            fontWeight: 'bold',
-            transition: 'background-color 0.3s ease',
-            '&:hover': {
-              backgroundColor: '#718096', // Hover più chiaro
-            },
-          }}
         >
           {testoButton}
         </Button>
       </Box>
       <Box
-  sx={{
-    backgroundColor: '#2d3748', // Un altro sfondo scuro per la sezione
-    color: '#f7fafc',
-    padding: (theme) => theme.spacing(6),
-    textAlign: 'center',
-    position: 'absolute', // Imposta la posizione come assoluta
-    bottom: 0,           // Ancora al bordo inferiore del contenitore posizionato
-    left: 0,             // Si estende dal bordo sinistro
-    right: 0,            // Si estende fino al bordo destro
-    zIndex: 1,           // Assicura che sia sopra altri elementi se necessario
-  }}
->
-  <Typography variant="h6" gutterBottom>
-    <Trans id="tecnologie_che_utilizzo" />
-  </Typography>
-  <Grid container justifyContent="center" spacing={4}>
-    {technologies.map((tech) => (
-      <Grid item key={tech.name} xs={6} sm={4} md={2} lg={1}>
-        <IconButton color="inherit" disabled>
-          {tech.icon}
-        </IconButton>
-        <Typography variant="caption" display="block">
-          {tech.name}
+        sx={{
+          backgroundColor: '#2d3748', // Un altro sfondo scuro per la sezione
+          color: '#f7fafc',        
+          textAlign: 'center',
+          position: 'absolute', // Imposta la posizione come assoluta
+          bottom: 0,           // Ancora al bordo inferiore del contenitore posizionato
+          left: 0,             // Si estende dal bordo sinistro
+          right: 0,            // Si estende fino al bordo destro
+          zIndex: 1,           // Assicura che sia sopra altri elementi se necessario
+        }}
+      >
+        <Typography variant="h6" gutterBottom>
+          <Trans id="tecnologie_che_utilizzo" />
         </Typography>
-      </Grid>
-    ))}
-  </Grid>
-</Box>
+        <Grid container justifyContent="center" spacing={4}>
+          {technologies.map((tech) => (
+            <Grid item key={tech.name} xs={6} sm={4} md={2} lg={1}>
+              <IconButton color="inherit" disabled>
+                {tech.icon}
+              </IconButton>
+              <Typography variant="caption" display="block">
+                {tech.name}
+              </Typography>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </>
   );
 }
