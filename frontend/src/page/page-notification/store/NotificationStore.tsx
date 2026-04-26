4ee309beed67256787ffe992cb7fa2297d42a042
Matt7046 <matteo.santangelo@colorsdev.tech>
2026-04-26 15:26:55 +0200

    creazione sezione notifica

---
+++ frontend/src/page/page-notification/store/NotificationStore.tsx
@@ -0,0 +1,41 @@
+import { makeObservable, observable } from 'mobx';
+import BaseStore from '../../../general/structure/BaseStore';
+import { ActivityI } from '../../page-activity/Activity';
+
+export class NotificationStore extends BaseStore {
+
+
+  @observable pointsField = 0;
+  @observable emailField = '';
+  @observable activity = [] as ActivityI[];
+  @observable points = 0;
+
+  constructor() {
+    super(); // Inizializza la classe base
+    makeObservable(this);
+
+  }
+
+  getStore() {
+    return this;
+  }
+  setPoints(points: number) {
+    this.points = points;
+  }
+  setPointsField(pointsField: number) {
+    this.pointsField = pointsField;
+  }
+  setEmailField(emailField: string) {
+    this.emailField = emailField;
+  }
+  setActivity(points: ActivityI[]) {
+    this.activity = points;
+  }
+}
+const notificationStore = new NotificationStore();
+export default notificationStore;
+
+
+
+
+
